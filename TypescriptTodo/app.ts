2bf2b6aa17e3ed9a3a36772d680b16a39c9e667f
jcbcw <diannasinclar@aol.com>
2017-09-28 15:51:46 -0400

    Implement type unions

--- TypescriptTodo/app.ts
+++ TypescriptTodo/app.ts
@@ -1,2 +1,14 @@
-let todo: string = "Pick up drycleaning";
-let name: string = "Jess";
+function totalLength(x: (string | any[]), y: (string | any[])): number {
+    var total: number = x.length + y.length;
+    
+    x.slice(0)
+    
+    if(x instanceof Array) {
+        x.push('TypeScript')
+    }
+    
+    if(x instanceof String) {
+        x.substr(0)
+    }
+    
+    return total;
