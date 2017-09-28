63e259e050d240493637fb680dcd411ab419e36e
jcbcw <diannasinclar@aol.com>
2017-09-28 15:18:26 -0400

    Implement use of arrow functions to bind local scope of the this property

--- TypescriptTodo/app.js
+++ TypescriptTodo/app.js
@@ -1,2 +1,9 @@
-var todo = "Pick up drycleaning";
-var name = "Jess";
+var container = document.getElementById('container');
+function Counter(el) {
+    var _this = this;
+    this.count = 0;
+    el.innerHTML = this.count;
+    el.addEventListener('click', function (event) { return el.innerHTML = (_this.count += 1); });
+}
+new Counter(container);
+var filtered = [1, 2, 3].filter(function (x) { return x > 0; });
